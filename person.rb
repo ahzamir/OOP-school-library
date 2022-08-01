771c322ca676f32b4cf128c5b237bac135015592
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-01 20:04:47 +0430

    add the methode can_use_services

--- person.rb
+++ person.rb
@@ -19,4 +19,11 @@ class Person
         end
     end
 
+    def can_use_services
+        if @age < 18 || @parent_permission = true
+            true
+        else
+            false
+        end
+    end
 end
