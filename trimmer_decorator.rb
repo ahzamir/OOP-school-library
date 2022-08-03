6c6f6962e83dd3fb250635b3937e7a12414e45c4
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-03 23:36:58 +0430

    solving linters issues

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
-require "./decorator"
+require './decorator'
 
 class TrimmerDecorator < Decorator
-    def correct_name
-        @nameable.correct_name[0, 10] if @nameable.correct_name > 10
-    end
+  def correct_name
+    @nameable.correct_name[0, 10] if @nameable.correct_name > 10
+  end
 end
