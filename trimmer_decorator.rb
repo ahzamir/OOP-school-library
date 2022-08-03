f6e64ff5c71f9e702a3c5ca8137a4a1b62fb0411
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-03 23:35:39 +0430

    chnage the require module to the decorator module

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,6 +1,6 @@
 require "./decorator"
 
-class TrimmerDecorator < decorator
+class TrimmerDecorator < Decorator
     def correct_name
         @nameable.correct_name[0, 10] if @nameable.correct_name > 10
     end
