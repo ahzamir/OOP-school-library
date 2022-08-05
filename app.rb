ffe4acea732abd701e6c5a2016331da12d3c8017
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-05 15:00:59 +0430

    define the menu method

--- app.rb
+++ app.rb
@@ -11,4 +11,15 @@ class App
         @people = []
     end
 
+    def menu
+        puts ''
+        puts 'Please choose an option by entering a number:'
+        puts '1 - List all books 📚'
+        puts '2 - List all people 🧑🏼‍🤝‍🧑🏻'
+        puts '3 - Create a person 🧑‍🦱'
+        puts '4 - Create a book 📘'
+        puts '5 - Create a rental 📦'
+        puts '6 - List all rentals for a given id 📦🆔'
+        puts '7 - Exit 🚪'
+    end
 end
