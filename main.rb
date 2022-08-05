58a5ca7b424a2263a0c89c5f8bb09b724dcc6032
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-05 17:33:55 +0430

    solving linters issues

--- main.rb
+++ main.rb
@@ -2,10 +2,10 @@
 require './app'
 
 def main
-    puts 'Welcome to the School Library App 📚🙋‍♂️'
-    puts ''
-    app = App.new()
-    app.run
+  puts 'Welcome to the School Library App 📚🙋‍♂️'
+  puts ''
+  app = App.new
+  app.run
 end
 
 main
