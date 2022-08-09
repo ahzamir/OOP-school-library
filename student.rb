cd78dd8a3a4f791c81334ea68f793018d9ecd91b
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-09 22:15:39 +0430

    add a method for Student class to change the object format to json

--- student.rb
+++ student.rb
@@ -17,4 +17,14 @@ class Student < Person
     @classroom = classroom
     classroom.student.push(self) unless classroom.student.include?(self)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'name' => name,
+      'age' => age,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
 end
