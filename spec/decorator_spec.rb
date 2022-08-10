4c631d7836a73af684c5b0de1a9ecce32857baf4
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-10 21:52:53 +0430

    add some tests for Decorator class

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,1 +1,27 @@
 require 'spec_helper'
+
+require 'spec_helper'
+
+describe Decorator do 
+  before :each do
+    @decorator = Decorator.new('ahmad')
+  end
+
+  describe "#new" do
+    it "returns new decorator object" do
+      @decorator.should be_an_instance_of Decorator
+    end
+    it 'throws error when arguments less than 1' do
+       lambda { decorator.new() }.should raise_exception ArgumentError
+    end
+    it 'throws error when arguments more than 1' do
+       lambda { decorator.new('name', 'extra_argument') }.should raise_exception ArgumentError
+    end
+  end
+
+  describe "testing argument values" do
+    it "returns correct nameable" do
+      @decorator.nameable.should eql 'ahmad'
+    end
+  end
+end
--- test.rb
+++ test.rb
@@ -3,5 +3,7 @@ require './student'
 require './book'
 require './person'
 require 'json'
+require './decorator'
 
 person = Person.new(34,'any')
+
