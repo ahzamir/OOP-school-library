d189721b6be78938a00185c61a4b97bbdd8c9551
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-10 17:14:47 +0000

    create test files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require_relative '../book.rb'
+require_relative '../person.rb'
+require_relative '../rental.rb'
+require_relative '../student.rb'
+require_relative '../teacher.rb'
+require_relative '../decorator.rb'
+require_relative '../classroom.rb'
--- test.rb
+++ test.rb
@@ -1,12 +1,7 @@
 require './teacher'
 require './student'
 require './book'
-require './rental'
+require './person'
 require 'json'
 
-File.open('books.json') { |file| @books = JSON.parse(file.read) }
-puts @books
-File.open('people.json') { |file| @people = JSON.parse(file.read) }
-puts @people
-File.open('rentals.json') { |file| @rentals = JSON.parse(file.read) }
-puts @rentals
+person = Person.new(34,'any')
