7cb975ad25523a554a25a94581b56f437f5b3a84
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-10 17:00:44 +0000

    create classroom tests for new(),parameter value no arg

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Classroom do 
+  before :each do
+    @classroom = Classroom.new('label')
+  end
+
+  describe "#new" do
+    it "returns new classroom object" do
+      @classroom.should be_an_instance_of Classroom
+    end
+    it 'throws error when arguments less than 2' do
+       lambda { Classroom.new() }.should raise_exception ArgumentError
+    end
+    it 'throws error when arguments more than 2' do
+       lambda { Classroom.new('label','extra_argument') }.should raise_exception ArgumentError
+    end
+  end
+
+  describe "testing argument value" do
+    it "returns correct label" do
+      @classroom.label.should eql 'label'
+    end
+  end
+end
