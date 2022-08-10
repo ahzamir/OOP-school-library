5435ce8db25c03c54b8a3d5855155f0a7eb7965c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-10 22:07:36 +0430

    add some tests for Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,1 +1,32 @@
 require 'spec_helper'
+
+describe student do 
+  before :each do
+    @classroom = Classroom.new('any')
+    @student = Student.new(@classroom, 34, 'zamir')
+  end
+
+  describe "#new" do
+    it "returns new student object" do
+      @student.should be_an_instance_of Student
+    end
+    it 'throws error when arguments less than 3' do
+       lambda { Student.new() }.should raise_exception ArgumentError
+    end
+    it 'throws error when arguments more than 3' do
+       lambda { Student.new('classroom', 'age', 'any', 'extra_argument') }.should raise_exception ArgumentError
+    end
+  end
+
+  describe "testing argument values" do
+    it "returns correct age" do
+      @student.age.should eql 34
+    end
+    it "returns correct name" do
+      @student.name.should eql 'zamir'
+    end
+    it "returns correct classroom" do
+        @student.classroom.should eql @classroom
+      end
+  end
+end
